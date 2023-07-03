07761deda601b2a0b3f1c63100f87bd1cbeb28b8
trancanh1102 <40576239+trancanh1102@users.noreply.github.com>
2023-07-04 00:16:14 +0700

    Update SetupClient.sh

--- SetupClient.sh
+++ SetupClient.sh
@@ -1,10 +1,10 @@
-latestVersion=0.33.0
+latestVersion=0.50.0
 mkdir -p /tmp/frp
-wget -O /tmp/frp/frp.tar.gz https://github.com/fatedier/frp/releases/download/v${latestVersion}/frp_${latestVersion}_linux_arm.tar.gz
+wget -O /tmp/frp/frp.tar.gz https://github.com/fatedier/frp/releases/download/v${latestVersion}/frp_${latestVersion}_linux_amd64.tar.gz
 tar -C /tmp/frp -zxvf /tmp/frp/frp.tar.gz
-sudo cp /tmp/frp/frp_${latestVersion}_linux_arm/frpc /usr/bin
+sudo cp /tmp/frp/frp_${latestVersion}_linux_amd64/frpc /usr/bin
 chmod +x /usr/bin/frpc
 sudo mkdir -p /etc/frp
-sudo wget -O /etc/frp/frpc.ini https://raw.githubusercontent.com/laomingOfficial/RaspberryPi-frp/master/frpc.ini
-sudo wget -O /etc/systemd/system/frpc.service https://raw.githubusercontent.com/laomingOfficial/RaspberryPi-frp/master/frpc.service
+sudo wget -O /etc/frp/frpc.ini https://raw.githubusercontent.com/trancanh1102/RaspberryPi-frp/master/frpc.ini
+sudo wget -O /etc/systemd/system/frpc.service https://raw.githubusercontent.com/trancanh1102/RaspberryPi-frp/master/frpc.service
 rm -r /tmp/frp
